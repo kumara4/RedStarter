00d812d0578b511d919036226a01f3db7c1c679b
Ashmita Kumar <akumar13@gmu.edu>
2016-10-03 00:20:14 -0400

    set up categories in setup.html

--- public/js/setup.js
+++ public/js/setup.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+    $('#setupcategory').click(function ($e) {
+        $e.preventDefault();
+        console.log("setup category clicked");
+    });
     $('.showcategory').click(function ($e) {
+        $e.preventDefault();
         console.log("show category clicked");
     });
-
 
 });
