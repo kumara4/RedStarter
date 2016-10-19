0f7f2a9e26bcee250fb3989dd657c70a2c6b99ee
Henry Lopez <henryapez@gmail.com>
2016-10-19 15:47:18 -0400

    Added Jasmine set up files

--- public/js/login2.js
+++ public/js/login2.js
@@ -15,22 +15,33 @@ var LoginBox = React.createClass({
     mixins: [ReactFireMixin],
 
     getInitialState: function () {
-        return {user: "", pass: "", newUsers: []};
+        return {user: "", pass: "", newUsers: [], uerror: ["", "",""], display: "none"};
     },
 
     componentWillMount: function () {
-        var ref = firebase.database().ref("users");
-        this.bindAsArray(ref, "users");
+        var ref = firebase.database().ref("newUsers");
+        this.bindAsArray(ref, "newUsers");
     },
 
-    onChangeUser: function (e) {
-        this.setState({user: e.target.value});
-    },
 
-    onChangePass: function (e) {
-        this.setState({pass: e.target.value});
-    },
     validateLogin: function (e) {
+        var user = this.refs.LoginUsername.state.user;
+        var pass = this.refs.LoginPassword.state.pass;
+        var correct = false;
+        var usersRef = firebase.database().ref("newUsers");
+        usersRef.child(user).once('value', function (snapshot) {
+            console.log("");
+            correct = (snapshot.val() !== null && snapshot.val().password == pass && user==snapshot.val().username);
+            if (correct) {
+                document.location.href = "landing.html";
+
+            } else {
+                correct = false;
+            }
+        });
+        if (!correct) {
+            this.setState({display: "inline", uerror: ["has-error", "inputError1", "Username/Password Incorrect"]})
+        }
 
 
     },
@@ -42,23 +53,69 @@ var LoginBox = React.createClass({
 
     },
 
-
     render: function () {
         ++count;
 
         return (
 
             <div key={this.props.id} className="row text-center LoginBox">
-                <LoginUsername ref="LoginUsername"/>
-                <LoginPassword ref="LoginPassword"/>
-                <LoginSubmit ref="LoginSubmit" validateUser={this.validateLogin}/>
-                <LoginForgot ref="LoginForgot"/>
+                <LoginUsername error={this.state.uerror}  ref="LoginUsername"/>
+                <LoginPassword error={this.state.uerror}  ref="LoginPassword"/>
+
+                <div className="col-lg-12">
+                    <button className="btn btn-primary" onClick={this.validateLogin} id="login"> Login
+                    </button>
+                </div>
+
                 <SignupBox ref="SignupBox"/>
+
             </div >
         );
     }
 });
 
+
+var LoginPassword = React.createClass({
+    getInitialState: function () {
+        return {pass: ""};
+    },
+    onChangePass: function (e) {
+
+        e.preventDefault();
+        this.setState({pass: e.target.value});
+    },
+    render: function () {
+        var style={display: this.props.display};
+        return (
+            <div className={"col-lg-12 " + this.props.error[0]}>
+                <label className={this.props.error[1]}>Password</label>
+                    <input id="loginPW" className={this.props.error[1]} type="password" onChange={this.onChangePass}
+                           value={this.state.pass} placeholder="Enter Password"/>
+                    <p className="warning" style={style}>{this.props.error[2]}</p>
+            </div>
+        )
+    }
+});
+var LoginUsername = React.createClass({
+    getInitialState: function () {
+        return {user: ""};
+    },
+    onChangeUser: function (e) {
+        e.preventDefault();
+        this.setState({user: e.target.value});
+    },
+    render: function () {
+        var style={display: this.props.display};
+        return (
+            <div className={"col-lg-12 " + this.props.error[0]}>
+                <label className={this.props.error[1]}>Username: </label>
+                    <input id="loginUN" className={this.props.error[1]} onChange={this.onChangeUser} value={this.state.user}
+                                        type="text" placeholder="Username"/>
+                <p className="warning" style={style}>{this.props.error[2]}</p>
+            </div>
+        )
+    }
+});
 
 var LoginForgot = React.createClass({
 
@@ -70,56 +127,85 @@ var LoginForgot = React.createClass({
         )
     }
 });
-var LoginSubmit = React.createClass({
-    render: function () {
-        return (
-            <div className="col-lg-12">
-                <a href="landing.html">
-                    <button type="button" className="btn btn-primary" onClick={this.handleAdd} id="login"> Login
-                    </button>
-                </a>
-            </div>
-        )
-    }
-});
-var LoginPassword = React.createClass({
-    getInitialState: function () {
-        return {pass: ""};
-    },
-    render: function () {
-        return (
-            <div className="col-lg-12">
-                <label>Password: <input id="loginPW" type="password" onChange={this.onChangePass}
-                                        value={this.state.pass} placeholder="Enter Password"/></label>
-            </div>
-        )
-    }
-});
-var LoginUsername = React.createClass({
-    getInitialState: function () {
-        return {user: ""};
-    },
-    render: function () {
-        return (
-            <div className="col-lg-12">
-                <label>Username: <input id="loginUN" onChange={this.onChangeUser} value={this.state.user}
-                                        type="text" placeholder="Username"/></label>
-            </div>
-        )
-    }
-});
+
+
 var SignupBox = React.createClass({
+    mixins: [ReactFireMixin],
+
+    getInitialState: function () {
+        return {user: "", pass: "", newUsers: [], existserror: ["", "",""],
+            confirmpasserror: ["", "",""],
+            confirmemailerror: ["", "",""],
+            display: "none"};
+    },
+
+    componentWillMount: function () {
+        var ref = firebase.database().ref("newUsers");
+        this.bindAsArray(ref, "newUsers");
+    },
 
 
+    validateSignup: function (e) {
+        var user = this.refs.SignupCredentials.state.user;
+        var pass = this.refs.SignupCredentials.state.pass;
+        var cpass = this.refs.SignupCredentials.state.passconfirm;
+        var email = this.refs.SignupCredentials.state.email;
+        var cemail = this.refs.SignupCredentials.state.confirmemail;
+        var firstname = this.refs.SignupName.state.firstname;
+        var lastname = this.refs.SignupName.state.lastname;
+
+
+        var userexists = false;
+        var usersRef = firebase.database().ref("newUsers");
+        usersRef.child(user).once('value', function (snapshot) {
+            var exists = (snapshot.val() !== null);
+            if (exists) {
+                userexists = true;
+            }
+            if (userexists) {
+                console.log("user exists");
+                this.setState({display: "inline", existserror: ["has-error", "inputError1", "Username already exists"]})
+            }
+            else if(email != cemail){
+                console.log("email not same");
+                this.setState({display: "inline", confirmemailserror: ["has-error", "inputError1", "Emails do not match"]})
+            }
+            else if(pass != cpass){
+                console.log("pass not same");
+                this.setState({display: "inline", confirmpasserror: ["has-error", "inputError1", "Passwords do not match"]})
+            }else {
+
+        usersRef.child(user.toString()).set(
+         {'firstname': firstname,
+            'lastname': lastname,
+            'email': email,
+            'username': user,
+            'password': pass,
+         });
+                document.location.href = "landing.html";
+        }
+
+
+        },this);
+
+
+    },
     render: function () {
         ++count;
 
         return (
             <div className="signup-section">
-                <SignupName />
-                <SignupUsername />
-                <SignupEmail />
-                <SignPassword />
+                <SignupName  ref="SignupName"/>
+
+                <SignupCredentials  ref="SignupCredentials" display={this.state.display} existserror={this.state.existserror} confirmpasserror={this.state.confirmpasserror} confirmemailerror={this.state.confirmemailerror} />
+
+                <div className="col-lg-12">
+
+                        <button type="button" className="btn btn-primary" onClick={this.validateSignup} id="signupsubmit"> Signup
+                        </button>
+
+                </div>
+
 
             </div>
 
@@ -133,70 +219,96 @@ var SignupBox = React.createClass({
 
 var SignupName = React.createClass({
     getInitialState: function () {
-        return {name: ""};
+        return {lastname:"", firstname: ""};
+    },
+    onChangefirst: function (e) {
+        e.preventDefault();
+        this.setState({firstname: e.target.value});
+    },
+    onChangelast: function (e) {
+        e.preventDefault();
+        this.setState({lastname: e.target.value});
     },
     render: function () {
         return (
             <div className="fullname-fields">
                 <div className="firstname">
                     <label>First Name:</label>
-                    <input id="name1" type="text" placeholder="First Name"/>
+                    <input id="name1" type="text" onChange={this.onChangefirst} value={this.state.value}placeholder="First Name"/>
                 </div>
                 <div className="lastname">
                     <label>Last Name:</label>
-                    <input id="name2" type="text" placeholder="Last Name"/>
+                    <input id="name2" type="text" onChange={this.onChangelast} value={this.state.value} placeholder="Last Name"/>
                 </div>
             </div>
         )
     }
 });
-var SignupEmail = React.createClass({
 
-    render: function () {
-        return (
-            <div className="email-fields">
-                <div className="row">
-                    <label>Email:</label>
-                    <input id="email" type="email" placeholder="Email"/>
-                </div>
-                <div className="row">
-                    <label>Confirm Email:</label>
-                    <input id="cemail" type="email" placeholder="Confirm Email"/>
 
-                </div>
-            </div>
-        )
-    }
-});
-var SignPassword = React.createClass({
+var SignupCredentials = React.createClass({
+    getInitialState: function () {
+        return {email:"",confirmemail:"",user: "", pass:"", passconfirm:""};
+    },
+    onChangeUser: function (e) {
+        e.preventDefault();
+        this.setState({user: e.target.value});
+    },
+    onChangePass: function (e) {
+        e.preventDefault();
+        this.setState({pass: e.target.value});
+    },
+    onChangePassCon: function (e) {
+        e.preventDefault();
+        this.setState({passconfirm: e.target.value});
+    },
+    onChangeEmail: function (e) {
+        e.preventDefault();
+        this.setState({email: e.target.value});
+    },
+    onChangeConEmail: function (e) {
+        e.preventDefault();
+        this.setState({confirmemail: e.target.value});
+    },
     render: function () {
+        var style={display: this.props.display};
         return (
-            <div className="password-fields">
-                <div className="row">
-                    <label>Password</label>
-                    <input id="password" placeholder="Password"/>
+            <div className="username-pass-fields">
+                <div className={"row "+this.props.existserror[0]}>
+                    <label className={this.props.existserror[1]}>Username</label>
+                    <input id="username" className={this.props.existserror[1]} onChange={this.onChangeUser} value={this.state.value} placeholder="Username"/>
+                    <p className="warning" style={style}>{this.props.existserror[2]}</p>
                 </div>
-                <div className="row">
-                    <label>Confirm Password</label>
-                    <input id="cpassword" placeholder="Confirm Password"/>
+                <div className="password-fields">
+                    <div className={"row "+this.props.confirmpasserror[0]}>
+                        <label className={this.props.confirmpasserror[1]}>Password</label>
+                        <input id="password" className={this.props.confirmpasserror[1]} onChange={this.onChangePass} value={this.state.value} placeholder="Password"/>
+                        <p className="warning" style={style}>{this.props.confirmpasserror[2]}</p>
+                    </div>
+                    <div className={"row "+this.props.confirmpasserror[0]}>
+                        <label className={this.props.confirmpasserror[1]}>Confirm Password</label>
+                        <input id="cpassword"  className={this.props.confirmpasserror[1]} onChange={this.onChangePassCon} value={this.state.value} placeholder="Confirm Password"/>
 
+
+                    </div>
+                    <div className={"row "+this.props.confirmemailerror[0]}>
+                        <label className={this.props.confirmemailerror[1]}>Email:</label>
+                        <input id="email" type="email" className={this.props.confirmemailerror[1]} onChange={this.onChangeEmail} value={this.state.value} placeholder="Email"/>
+                        <p className="warning" style={style}>{this.props.confirmemailerror[2]}</p>
+                    </div>
+                    <div className="row">
+                        <label className={this.props.confirmemailerror[1]}>Confirm Email:</label>
+                        <input id="cemail" type="email"  className={this.props.confirmemailerror[1]} onChange={this.onChangeConEmail} value={this.state.value} placeholder="Confirm Email"/>
+
+                    </div>
                 </div>
             </div>
+
         )
     }
 });
-var SignupUsername = React.createClass({
-    render: function () {
-        return (
-            <div className="username-fields">
-                <div className="row">
-                    <label>Username</label>
-                    <input id="username" className="username" placeholder="Username"/>
-                </div>
-            </div>
-        )
-    }
-});
+
+
 
 
 // tutorial4.js
