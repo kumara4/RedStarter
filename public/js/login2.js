9d57d22beab0564bb40d9688c07b9297e31944d1
Ashmita Kumar <akumar13@gmu.edu>
2016-10-31 01:41:56 -0400

    resolved all merge conflicts from gh-pages

--- public/js/login2.js
+++ public/js/login2.js
@@ -38,40 +38,6 @@ var LoginBox = React.createClass({
         var user = this.state.user;
         var pass = this.state.pass;
         var correct = false;
-<<<<<<< HEAD
-        // this.usersRef = firebase.database().ref("newUsers");
-        // this.usersRef.child(user).once('value', function (snapshot) {
-        //     console.log("");
-        //     correct = (snapshot.val() !== null && snapshot.val().info.password == pass && user == snapshot.val().info.username);
-        //     if (correct) {
-        //         document.location.href = "landing.html";
-        //
-        //     } else {
-        //         this.setState({display: "inline", uerror: ["has-error", "inputError1", "Username/Password Incorrect"]})
-        //     }
-        // }.bind(this));
-        $.get("http://localhost:3000/login", {user: user, pass: pass}).then(function (data) {
-            console.log("THE URL IS " + data.url);
-
-
-            document.location.href = data.url;
-        });
-        // $.ajax({url: "/login",
-        //     type: 'GET',
-        //     data: {user: user, pass:pass },
-        //     success: function (output) {
-        //
-        //        if(output.url == null || output.url.equals("")){
-        //            this.setState({display: "inline", uerror: ["has-error", "inputError1", "Username/Password Incorrect"]});
-        //        }else{
-        //            document.location.href = output.url;
-        //        }
-        //
-        //
-        //
-        //     }.bind(this)
-        // });
-=======
         $.get("/login", {user: user, pass: pass}).then(function (data) {
             console.log("THE URL IS " + data.url);
 
@@ -81,7 +47,6 @@ var LoginBox = React.createClass({
                 document.location.href = data.url;}
         }.bind(this));
 
->>>>>>> gh-pages
 
     },
     handleAdd: function (e) {
@@ -125,21 +90,6 @@ var LoginBox = React.createClass({
 });
 
 
-<<<<<<< HEAD
-var LoginForgot = React.createClass({
-
-    render: function () {
-        return (
-            <div className="col-lg-12">
-                <button type="button" className="btn btn-primary" id="forgotpw">Forgot Password</button>
-            </div>
-        )
-    }
-});
-
-
-=======
->>>>>>> gh-pages
 var SignupBox = React.createClass({
     mixins: [ReactFireMixin],
 
@@ -182,10 +132,7 @@ var SignupBox = React.createClass({
 
         }
         if (user != "") {
-<<<<<<< HEAD
-=======
             console.log("ENTERED BEFORE USEREF");
->>>>>>> gh-pages
             this.usersRef.child(user).once('value', function (snapshot) {
                 var exists = (snapshot.val() !== null);
                 if (exists) {
@@ -215,28 +162,6 @@ var SignupBox = React.createClass({
                     alert("You must fill in all fields");
 
                 } else {
-<<<<<<< HEAD
-                    var newUser = {
-                        'firstname': firstname,
-                        'lastname': lastname,
-                        'email': email,
-                        'username': user,
-                        'password': pass,
-                    };
-                    $.post("http://localhost:3000/signup", {user: user, newuser: newUser}).then(function (data) {
-                        window.location = data.redirectUrl;
-                    });
-
-                    // this.usersRef.child(user.toString()).set(
-                    //     {
-                    //         'firstname': firstname,
-                    //         'lastname': lastname,
-                    //         'email': email,
-                    //         'username': user,
-                    //         'password': pass,
-                    //     });
-                    // document.location.href = "landing.html";
-=======
 
 
                     var formData = new FormData($("#newItemForm")[0]);
@@ -253,7 +178,6 @@ var SignupBox = React.createClass({
                     });
 
 
->>>>>>> gh-pages
                 }
 
 
@@ -266,21 +190,6 @@ var SignupBox = React.createClass({
         ++count;
         return (
             <div className="signup-section">
-<<<<<<< HEAD
-                <SignupName ref="SignupName"/>
-
-                <SignupCredentials ref="SignupCredentials" display={this.state.display}
-                                   existserror={this.state.existserror}
-                                   confirmpasserror={this.state.confirmpasserror}
-                                   confirmemailerror={this.state.confirmemailerror}/>
-
-                <div className="col-lg-12">
-
-                    <button type="button" className="btn btn-primary" onClick={this.validateSignup}
-                            id="signupsubmit">Signup
-                    </button>
-
-=======
                 <div className="col-lg-12">
                     <form id="newItemForm" encType="multipart/form-data">
                         <SignupName ref="SignupName"/>
@@ -292,7 +201,6 @@ var SignupBox = React.createClass({
                         <input className="btn btn-primary" onClick={this.validateSignup} id="signupsubmit" type="button"
                                value="Signup"/>
                     </form>
->>>>>>> gh-pages
                 </div>
             </div>
         );
@@ -317,21 +225,13 @@ var SignupName = React.createClass({
             <div className="fullname-fields">
                 <div className="firstname">
                     <label>First Name:</label>
-<<<<<<< HEAD
-                    <input id="name1" type="text" onChange={this.onChangefirst} value={this.state.value}
-=======
                     <input id="name1" name="firstname" type="text" onChange={this.onChangefirst}
                            value={this.state.value}
->>>>>>> gh-pages
                            placeholder="First Name"/>
                 </div>
                 <div className="lastname">
                     <label>Last Name:</label>
-<<<<<<< HEAD
-                    <input id="name2" type="text" onChange={this.onChangelast} value={this.state.value}
-=======
                     <input id="name2" name="lastname" type="text" onChange={this.onChangelast} value={this.state.value}
->>>>>>> gh-pages
                            placeholder="Last Name"/>
                 </div>
             </div>
@@ -364,52 +264,21 @@ var SignupCredentials = React.createClass({
         e.preventDefault();
         this.setState({confirmemail: e.target.value});
     },
-    onSubmit: function (e) {
-        var fireRef = firebase.database().ref('newUsers');
-        fireRef.on("child_added",function(v){
-            displayPic(v.val().img);
-        });
-        function displayPic(img)
-        {
-                $('#photo').append(
-                    '<div class="newPhoto"><img src="'+img+'" /><br/>'+text+'</div>'
-                );
-        }
-        e.preventDefault();
-        var formData = new FormData($("#newItemForm")[0]);
-        console.log(formData);
-        $.ajax({
-            type: "POST",
-            url: "/todo",
-            data: formData,processData: false,
-            contentType: false
-
-        });
-
-    },
     render: function () {
         var style = {display: this.props.display};
         return (
             <div className="username-pass-fields">
                 <div className={"row " + this.props.existserror[0]}>
                     <label className={this.props.existserror[1]}>Username</label>
-<<<<<<< HEAD
-                    <input id="username" className={this.props.existserror[1]} onChange={this.onChangeUser}
-=======
                     <input name="username" id="username" className={this.props.existserror[1]}
                            onChange={this.onChangeUser}
->>>>>>> gh-pages
                            value={this.state.value} placeholder="Username"/>
                     <p className="warning" style={style}>{this.props.existserror[2]}</p>
                 </div>
                 <div className="password-fields">
                     <div className={"row " + this.props.confirmpasserror[0]}>
                         <label className={this.props.confirmpasserror[1]}>Password</label>
-<<<<<<< HEAD
-                        <input id="password" className={this.props.confirmpasserror[1]}
-=======
                         <input name="password" id="password" className={this.props.confirmpasserror[1]}
->>>>>>> gh-pages
                                onChange={this.onChangePass} value={this.state.value} placeholder="Password"/>
                         <p className="warning" style={style}>{this.props.confirmpasserror[2]}</p>
                     </div>
@@ -423,11 +292,7 @@ var SignupCredentials = React.createClass({
                     </div>
                     <div className={"row " + this.props.confirmemailerror[0]}>
                         <label className={this.props.confirmemailerror[1]}>Email:</label>
-<<<<<<< HEAD
-                        <input id="email" type="email" className={this.props.confirmemailerror[1]}
-=======
                         <input id="email" name="email" type="email" className={this.props.confirmemailerror[1]}
->>>>>>> gh-pages
                                onChange={this.onChangeEmail} value={this.state.value} placeholder="Email"/>
                         <p className="warning" style={style}>{this.props.confirmemailerror[2]}</p>
                     </div>
@@ -438,12 +303,6 @@ var SignupCredentials = React.createClass({
                                placeholder="Confirm Email"/>
 
                     </div>
-<<<<<<< HEAD
-                    <div id="photo"></div>
-
-                    <form id="newItemForm" action="/todo" method="post" enctype="multipart/form-data"><input type="text" id="newTodo" name="todoText" /><input type="file" id="newFile" name="img" /><input onClick={this.onSubmit} type="submit" value="Upload Picture" /></form>
-=======
->>>>>>> gh-pages
 
                 </div>
             </div>
@@ -458,4 +317,3 @@ var SignupCredentials = React.createClass({
 ReactDOM.render(
     < LoginBox />, document.getElementById('signupbox')
 );
-
